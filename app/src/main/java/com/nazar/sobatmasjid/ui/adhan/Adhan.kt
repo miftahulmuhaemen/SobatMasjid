90f4cfb5a16f97ee3f4c12ecc6bc1cad7dada0d9
Miftahul Muhaemen <miftahulmuhaemen@gmail.com>
2020-09-19 17:43:44 +0800

    19/09/2020 Continue design.

---
+++ app/src/main/java/com/nazar/sobatmasjid/ui/adhan/Adhan.kt
@@ -0,0 +1,12 @@
+package com.nazar.sobatmasjid.ui.adhan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nazar.sobatmasjid.R
+
+class Adhan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adhan)
+    }
+}
---
+++ app/src/main/java/com/nazar/sobatmasjid/ui/locationn/Location.kt
@@ -0,0 +1,11 @@
+package com.nazar.sobatmasjid.ui.locationn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Location : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
