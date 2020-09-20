90875e4a36735e8ac1428a3d6c0b9a92a80cd7e8
Miftahul Muhaemen <miftahulmuhaemen@gmail.com>
2020-09-21 01:39:27 +0800

    21/09/2020 Continue design.

---
+++ app/src/main/java/com/nazar/sobatmasjid/ui/filter/Filter.kt
@@ -0,0 +1,12 @@
+package com.nazar.sobatmasjid.ui.filter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nazar.sobatmasjid.R
+
+class Filter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
